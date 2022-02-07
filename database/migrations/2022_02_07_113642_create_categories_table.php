7472cd43165f67194d8752eaa4626b4daa10169e
Dani9898 <danieleoliva98@gmail.com>
2022-02-07 17:08:36 +0100

    foreign key table

--- database/migrations/2022_02_07_113642_create_categories_table.php
+++ database/migrations/2022_02_07_113642_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,11 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table -> string('titolo');
-            $table -> string('autore');
-            $table -> string('sottotitolo');
-            $table -> text('contenuto');
-            $table -> date('data');
+            $table->string('name');
+            
             $table->timestamps();
-
-            // $table -> unsignedBigInteger('category_id') -> nullable();
-           
         });
     }
 
@@ -34,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 }
