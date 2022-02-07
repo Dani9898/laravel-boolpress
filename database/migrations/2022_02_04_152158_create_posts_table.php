21de83fea9c4ebda31886d4ae2ffae5d065d54bc
Dani9898 <danieleoliva98@gmail.com>
2022-02-07 17:23:36 +0100

    relazione tra posts e categories

--- database/migrations/2022_02_04_152158_create_posts_table.php
+++ database/migrations/2022_02_04_152158_create_posts_table.php
@@ -22,9 +22,12 @@ class CreatePostsTable extends Migration
             $table -> date('data');
             $table->timestamps();
 
-            // $table -> unsignedBigInteger('category_id') -> nullable();
+            $table -> unsignedBigInteger('category_id') -> nullable();
+            $table -> foreign('category_id') -> references('id') -> on('categories');
            
         });
+
+        
     }
 
     /**
